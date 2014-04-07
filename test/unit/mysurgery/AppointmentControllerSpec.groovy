477507fb72f798bc68c3eb5192bede9b88e9fe97
Jack Mason <student@debian>
2014-04-07 18:00:55 +0100

    Added patient view

---
+++ test/unit/mysurgery/AppointmentControllerSpec.groovy
@@ -0,0 +1,20 @@
+package mysurgery
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(AppointmentController)
+class AppointmentControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
